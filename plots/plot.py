efd66ae8ca6753002d8d7e3e66f7d3e998e9b4c3
nhinojos <noah.hinojos@gmail.com>
2022-05-24 22:44:30 -0700

    Cleanup

--- plots/plot.py
+++ plots/plot.py
@@ -1,4 +1,4 @@
-# Importing math-related libraries
+# Importing math-related libraries.
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -30,12 +30,12 @@ def f_fall(x, xi_fall, N, tau):
 def f_fit(x, A, s, 
           xi_voigt=0, alpha=None, gamma=None,
           xi_fall=0, N=None, tau=None):
-    sigma = alpha / np.sqrt(2 * np.log(2))
+    sigma = alpha / np.sqrt(2*np.log(2))
     return A*(  (1 - s)*f_fall(x, xi_fall, N, tau) 
               + s*f_voigt(x - xi_voigt, sigma, gamma)  )
 
 
-## Finding parameters to fit curve
+## Finding parameters to fit curve.
 # Every x value represents the middle point of each histogram bin.
 x_vals = []
 for i in range(len(imass_hist[1]) - 1):
@@ -43,12 +43,12 @@ for i in range(len(imass_hist[1]) - 1):
     x_vals.append(avg)
 # Every y value is simple the size of the size/height of the histogram bin. 
 y_vals = imass_hist[0]
-# Calculating parameters using curve_fit()
-param = curve_fit(f_fit, 
-                      x_vals,
-                      y_vals,
-                      p0=[1, 1, 91, 1, 1, 69, 1, 1], # Guessing the x-shift of each function as 90.7 and 68.
-                      maxfev=3000)[0]
+# Calculating parameters using curve_fit().
+param = curve_fit(f_fit,                            
+                  x_vals,                          
+                  y_vals,                             
+                  p0=[1, 1, 91, 1, 1, 69, 1, 1], # Guessing the x-shift of each function as 91 and 69.
+                  maxfev=3000)[0]                 
 # Printing calculated parameters.
 print(100*"*")
 print("Calculated Parameters:")
@@ -59,10 +59,10 @@ print(param)
 print(100*"*")
 
 
-## Plotting fitted function to histogram
-# Alterting list of x values to be a numpy array for plotting
+## Plotting fitted function to histogram.
+# Alterting list of x values to be a numpy array for plotting.
 x_vals = np.asarray(x_vals)
-# Plotting the function, given the x values and parameters. 
+# Plotting the function given the x values and parameters. 
 plt.plot(x_vals, f_fit(x_vals,*param),
          "black", linestyle='dashed',label="Sig.+Bg.")
 plt.plot(x_vals, param[1]*(1-param[2])*f_fall(x_vals,*param[-3:]),
