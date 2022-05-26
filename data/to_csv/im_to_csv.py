Fixed Inv. Mass Calculation. Electron mass -> Muon mass. Results appear unaffected.

data/to_csv/im_to_csv.py
@@ -5,8 +5,8 @@ import pandas as pd
 ## Defining function that calculates invariant mass.
 def invariant_mass(pT1,phi1,eta1,pT2,phi2,eta2):
     
-    # Electron mass in GeV
-    mass_e = .510998950 * (10**-3)
+    # Muon mass in GeV/c^2
+    mass_m = 105.6583755 * (10**-3)
     
     # Momentum Vectors.
     p1 = [ pT1*np.cos(phi1), pT1*np.sin(phi1), pT1*np.sinh(eta1) ]
@@ -14,11 +14,11 @@ def invariant_mass(pT1,phi1,eta1,pT2,phi2,eta2):
     pdot = p1[0]*p2[0] + p1[1]*p2[1] + p1[2]*p2[2]
     
     # Energies.
-    E1 = np.sqrt( (mass_e**2) + (p1[0]**2) + (p1[1]**2) + (p1[2]**2) )
-    E2 = np.sqrt( (mass_e**2) + (p2[0]**2) + (p2[1]**2) + (p2[2]**2) )
+    E1 = np.sqrt( (mass_m**2) + (p1[0]**2) + (p1[1]**2) + (p1[2]**2) )
+    E2 = np.sqrt( (mass_m**2) + (p2[0]**2) + (p2[1]**2) + (p2[2]**2) )
     
     # Invariant Mass Calulation.
-    return np.sqrt( 2*(mass_e**2 + E1*E2 - pdot) )
+    return np.sqrt( 2*(mass_m**2 + E1*E2 - pdot) )
 
 
 ## Calcualating invariant mass and writing to csv.