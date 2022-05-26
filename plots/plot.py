8f9145c04c33d3c3c5430a00e8d70f45719ca41d
nhinojos <noah.hinojos@gmail.com>
2022-05-26 16:42:46 -0700

    Added Signal Variables to Histogram. Best iso. bound is .084.

--- plots/plot.py
+++ plots/plot.py
@@ -10,7 +10,7 @@ print(100 * "*")
 # Only keeping values within specific isolation boundaries.
 muon_df = pd.read_csv(open("data\\mini_muons.csv"), index_col=0)
 imass_df = pd.read_csv(open("data\\invariant_mass.csv"), index_col=0)
-iso_bounds = (0, 5)
+iso_bounds = (0, .084)
 imass_df[["Iso 1", "Iso 2"]] = muon_df[["Iso 1", "Iso 2"]]
 imass_df = pd.merge(imass_df[(imass_df["Iso 1"] > iso_bounds[0])
                              & (imass_df["Iso 1"] < iso_bounds[1])],
@@ -25,18 +25,19 @@ print(imass_df)
 print(100 * "-")
 
 
-### Histogram generation.
+# Histogram generation.
 # Defining histogram parameters.
 bins = 80
 rng = (70, 110)
-## Plotting and design.
-# Retrieving y-ticks from non-normalized histogram. 
+# Plotting and design.
+# Retrieving y-ticks from non-normalized histogram.
 imass_hist = plt.hist(imass_df["Invariant Mass"],
                       bins=bins,
                       range=rng,
                       color="firebrick",
                       label="Muon IM")
-ytck_nonn = [int(y) for y in plt.yticks()[0]] #Integer labels of original y-ticks
+# Integer labels of original y-ticks
+ytck_nonn = [int(y) for y in plt.yticks()[0]]
 plt.clf()
 
 # Recreating histogram, yet now normalized.
@@ -45,14 +46,14 @@ imass_hist = plt.hist(imass_df["Invariant Mass"],
                       range=rng,
                       density=True,  # Normalization Parameter.
                       color="firebrick",
-                      label="Muon IM")
-plt.yticks(np.linspace(0, plt.yticks()[0][-1], num=len(ytck_nonn)),ytck_nonn)
-plt.title("Inv. Mass, Normalized")
+                      label="OS Muons")
+plt.yticks(np.linspace(0, plt.yticks()[0][-1], num=len(ytck_nonn)), ytck_nonn)
+plt.title("Muons Invariant Mass — Isolation <" + str(iso_bounds[1]))
 plt.xlabel("Mass [GeV]")
 plt.ylabel("Entities / bin")
 
 
-## x,y values for curve-fitting.
+# x,y values for curve-fitting.
 # x_vals represents the middle points of each histogram bin.
 x_vals = []
 for i in range(len(imass_hist[1]) - 1):
@@ -101,6 +102,9 @@ print(100 * "-")
                              bounds=para_bounds,
                              maxfev=10000)
 
+signal_fraction = para[0]
+signal_uncertain = np.sqrt(para_var[0, 0])
+
 print("Calculated Parameters:")
 print("[s, tau,  alpha, gamma")
 print("xi_fall, xi_voight]")
@@ -112,18 +116,31 @@ print("Paramater Variance Matrix:")
 print(para_var)
 print(100 * "-")
 
-# Plotting fitted function to histogram.
+# Adding functions to histogram
 # Alterting list of x values to be a numpy array for plotting.
 x_vals = np.asarray(x_vals)
 # Plotting the function given the x values and parameters.
 plt.plot(x_vals, f_fit(x_vals, *para),
-         "black", linestyle='dashed', label="Sig.+Bg.")
+         "black", linestyle='dashed', label="Sig.+Backg.")
 plt.plot(x_vals, (1 - para[0]) * f_fall(x_vals, para[2], para[5]),
-         "lightsalmon", linestyle='dotted', label="Backg.")
-plt.legend()
+         "lightsalmon", linestyle='dotted', label="Background")
+# Adding signal fraction & uncertainty to histogram
+# Conditional string for signal fraction because it may be rounded up to 1.0
+if signal_fraction <= .994:
+    signal_fraction_string = "{:.2f}".format(signal_fraction)[1:]
+else:
+    signal_fraction_string = ".99"
+# Adding signal fraction & uncertainty to legend.
+plt.legend(title="Sig. Frac. = "
+           + signal_fraction_string
+           + "±"
+           + "{:.3f}".format(signal_uncertain)[1:])
+
+
 # Saving histogram.
 plt.savefig("plots\\muon_hist.png")
 
-print("Signal Fraction:", para[0])
-print("SF Uncertainty:", np.sqrt(para_var[0, 0]))
+
+print("Signal Fraction:", signal_fraction)
+print("SF Uncertainty:", signal_uncertain)
 print(100 * "-")
